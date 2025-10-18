a4cfa36f4b8ee92dea62d909d140c4516cfd5c9b
Pratik D <tiptoppratik@gmail.com>
2025-10-18 10:33:54 +0100

    feat: self signed ssl certificate and https server

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,27 @@ import { WebSocketServer } from "ws";
 import { GameManager } from "./GameManager";
 import { appRouter } from "./routes.ts";
 import { WebSocketManager } from "./WebSocketManager";
+import fs from "fs";
+import https, { ServerOptions } from "https";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 443;
 
-const server = http.createServer(app);
+const options: ServerOptions = {
+  key: fs.readFileSync(process.env.SSL_KEY!),
+  cert: fs.readFileSync(process.env.SSL_CERT!),
+};
+
+const server = https.createServer(options, app);
 const wss = new WebSocketServer({ server });
 const eventEmitter = new EventEmitter();
 export const gameManager = new GameManager(eventEmitter);
 export const socketManager = new WebSocketManager(wss, eventEmitter);
 
-app.use("/api/v1", appRouter)
+app.use("/api/v1", appRouter);
 
 server.listen(PORT, () => {
   console.log("Listening on port", PORT);
