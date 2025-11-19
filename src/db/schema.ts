d69474e68744bed6be1a9f54ffa9a030eec65f20
Pratik D <tiptoppratik@gmail.com>
2025-11-19 23:46:14 +0000

    refactor: remove unique value constraint for country column

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,5 +4,5 @@ export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email_id: varchar({ length: 255 }).notNull(),
   username: varchar({ length: 255 }).unique(),
-  country: varchar({ length: 3 }).unique(),
+  country: varchar({ length: 3 }),
 });
